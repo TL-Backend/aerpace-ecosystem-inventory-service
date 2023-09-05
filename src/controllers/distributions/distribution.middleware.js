41be1640be4c1fe99c1871087494ad0501c39702
vikas143516 <vikas@tilicho.in>
2023-09-05 18:06:47 +0530

    refactoring code

--- src/controllers/distributions/distribution.middleware.js
+++ src/controllers/distributions/distribution.middleware.js
@@ -3,73 +3,150 @@ const { logger } = require('../../utils/logger');
 const { errorResponse } = require('../../utils/responseHandler');
 const messages = require('./distribution.constant');
 
-const validateInput = (input, validations) => {
-  const errorsList = [];
-
-  for (const key in validations) {
-    const value = input[key];
-    const validation = validations[key];
-
-    if (value === undefined || value === null || !validation(value)) {
-      errorsList.push(
-        messages.errorMessages.INVAILD_STRING_OR_MISSING_ERROR(key),
-      );
-    }
-  }
-
-  return errorsList;
-};
-
-const emailValidation = (value) => {
-  // Add your email validation logic here
-  const validRegex =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  return value && value.trim().match(validRegex);
-};
-
-const stringValidation = (value) =>
-  typeof value === 'string' && value.trim().length > 0;
-
 exports.validateDistributionInput = async (req, res, next) => {
   try {
     const {
+      distribution_name,
+      distribution_email,
+      distribution_region,
+      distribution_phone_number,
+      distribution_address,
+      distribution_country_code,
       distributor_first_name,
       distributor_last_name,
       distributor_country_code,
-      distributor_state,
       distributor_phone_number,
       distributor_email,
+      distributor_address,
       distributor_pin_code,
-      distribution_name,
-      distribution_region,
-      distribution_phone_number,
-      distribution_email,
-      distribution_address,
-      distribution_country_code,
+      distributor_state,
     } = req.body;
-
-    const validations = {
-      distributor_first_name: stringValidation,
-      distributor_last_name: stringValidation,
-      distributor_country_code: stringValidation,
-      distributor_state: stringValidation,
-      distributor_phone_number: stringValidation,
-      distributor_email: emailValidation,
-      distributor_pin_code: stringValidation,
-      distribution_name: stringValidation,
-      distribution_region: stringValidation,
-      distribution_phone_number: stringValidation,
-      distribution_email: emailValidation,
-      distribution_address: stringValidation,
-      distribution_country_code: stringValidation,
-    };
-
-    const errorsList = validateInput(req.body, validations);
-
-    if (errorsList.length) {
-      throw errorsList.join(', ');
+    const errorsList = [];
+    if (
+      !distributor_first_name?.trim() ||
+      typeof distributor_first_name !== 'string'
+    ) {
+      errorsList.push(
+        messages.errorMessages.INVAILD_STRING_OR_MISSING_ERROR(
+          'distributor_first_name',
+        ),
+      );
     }
-
+    if (
+      !distribution_region?.trim() ||
+      typeof distribution_region !== 'string'
+    ) {
+      errorsList.push(
+        messages.errorMessages.INVAILD_STRING_OR_MISSING_ERROR(
+          'distribution_region',
+        ),
+      );
+    }
+    if (!distribution_name?.trim() || typeof distribution_name !== 'string') {
+      errorsList.push(
+        messages.errorMessages.INVAILD_STRING_OR_MISSING_ERROR(
+          'distribution_name',
+        ),
+      );
+    }
+    if (
+      !distributor_last_name?.trim() ||
+      typeof distributor_last_name !== 'string'
+    ) {
+      errorsList.push(
+        messages.errorMessages.INVAILD_STRING_OR_MISSING_ERROR(
+          'distributor_last_name',
+        ),
+      );
+    }
+    if (
+      !distributor_country_code?.trim() ||
+      typeof distributor_country_code !== 'string'
+    ) {
+      errorsList.push(
+        messages.errorMessages.INVAILD_STRING_OR_MISSING_ERROR(
+          'distributor_country_code',
+        ),
+      );
+    }
+    if (!distributor_state?.trim() || typeof distributor_state !== 'string') {
+      errorsList.push(
+        messages.errorMessages.INVAILD_STRING_OR_MISSING_ERROR(
+          'distributor_state',
+        ),
+      );
+    }
+    if (
+      !distributor_phone_number?.trim() ||
+      typeof distributor_phone_number !== 'string'
+    ) {
+      errorsList.push(
+        messages.errorMessages.INVAILD_STRING_OR_MISSING_ERROR(
+          'distributor_phone_number',
+        ),
+      );
+    }
+    if (
+      !distribution_phone_number?.trim() ||
+      typeof distribution_phone_number !== 'string'
+    ) {
+      errorsList.push(
+        messages.errorMessages.INVAILD_STRING_OR_MISSING_ERROR(
+          'distribution_phone_number',
+        ),
+      );
+    }
+    let validRegex =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!distributor_email?.trim() || !distributor_email.match(validRegex)) {
+      errorsList.push(messages.errorMessages.INVAILD_EMAIL_FORMAT_MESSAGE);
+    }
+    if (!distribution_email?.trim() || !distribution_email.match(validRegex)) {
+      errorsList.push(messages.errorMessages.INVAILD_EMAIL_FORMAT_MESSAGE);
+    }
+    if (
+      !distributor_pin_code?.trim() ||
+      typeof distributor_pin_code !== 'string'
+    ) {
+      errorsList.push(
+        messages.errorMessages.INVAILD_STRING_OR_MISSING_ERROR(
+          'distributor_pin_code',
+        ),
+      );
+    }
+    if (
+      !distribution_country_code?.trim() ||
+      typeof distribution_country_code !== 'string'
+    ) {
+      errorsList.push(
+        messages.errorMessages.INVAILD_STRING_OR_MISSING_ERROR(
+          'distribution_country_code',
+        ),
+      );
+    }
+    if (
+      !distributor_address?.trim() ||
+      typeof distributor_address !== 'string'
+    ) {
+      errorsList.push(
+        messages.errorMessages.INVAILD_STRING_OR_MISSING_ERROR(
+          'distributor_address',
+        ),
+      );
+    }
+    if (
+      !distribution_address?.trim() ||
+      typeof distribution_address !== 'string'
+    ) {
+      errorsList.push(
+        messages.errorMessages.INVAILD_STRING_OR_MISSING_ERROR(
+          'distribution_address',
+        ),
+      );
+    }
+    if (errorsList.length) {
+      throw errorsList.join(' ,');
+    }
     return next();
   } catch (error) {
     logger.error(error);
