51583cd9d11487d51bc51262f97de88d6d87f4bd
Pradeep-p73 <pradeep@tilicho.in>
2023-09-25 10:27:08 +0530

    Updated constants

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -19,11 +19,6 @@ exports.successResponses = {
 exports.errorResponses = {
   FAILURE: `Failed to perform the operation`,
 };
-exports.dbTables = {
-  DEVICES_IMPORT_HISTORY: 'aergov_device_import_histories',
-  DEVICE_VERSION_TABLE: 'aergov_device_versions',
-  DEVICES_TABLE: 'aergov_devices',
-};
 
 exports.levelStarting = {
   version: 'ver_',
